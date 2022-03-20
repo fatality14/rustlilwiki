fd05a361da3c4a017ca8d106e6666797b6bb2378
zhenya11114 <60868827+zhenya11114@users.noreply.github.com>
2022-03-20 12:38:20 +0300

    Add files via upload

--- main.rs
+++ main.rs
@@ -27,7 +27,7 @@ fn main() {
 
     //0
     //variable casts to type while writing (&), (&mut) etc near
-    //also it might be casted in the same way using (as)
+    //also it might be casted in the same way using (as) especially when we cast to a different type
     //we can cast pointer types back with (*T)
     //we can cast raw pointer types back with unsafe (*T) or methor as_ref
 
@@ -49,7 +49,9 @@ fn main() {
 
 
     // let x = 5;
-    // x = 6;
+    // // x = 6;
+    // let mut xm = x;
+    // xm = 6;
 
     // let mut a = 1;
     // let b: *mut i32 = &mut a;
@@ -57,9 +59,11 @@ fn main() {
     // let mut c = a;
     // let d = &mut c;
 
-    //4
+    //4.1
     //a variable must be (mut) if it could change
     //else it is constant
+    //4.2
+    //we can redeclare non(mut) variable as (mut) if there is no other borrowing operations
 
     //5
     //(&mut) type variable could only be assigned if assignable variable is marked (mut)
@@ -75,18 +79,38 @@ fn main() {
     //there is no (mut) fields
 
 
+    // fn foo(a: &mut &i32, b: &mut &i32){}
+
     // let mut s = &3;
     
     // let r1 = &mut s;
     // let r2 = &mut s;
 
-    // println!("{}, {}", r1, r2);
+    // foo(r1,r2);
 
-    //7
+    // fn foo1(a: *mut &i32, b: *mut &i32){}
+    
+    // let mut s = &3;
+    
+    // let r1: *mut &i32 = &mut s;
+    // let r2: *mut &i32 = &mut s;
+
+    // foo1(r1,r2);
+
+    //7.1
     //it must be one (&mut T | &mut &T) at a time if the variable is planned to be used
     //or any of (&T) at a time
     //if no (&) or (&mut) sepcified the move operation occurs
     //the move operation steals a pointer from variable
+    //note that (*mut T | *mut &T) allows you to obey this rule
+
+    
+    // let s = "123".to_string();
+    // let s1 = "123";
+    // let s2 = s + s1;
+
+    //n
+    //TODO add rule here about move in operators
 
 
     // struct Test(i32);
@@ -172,6 +196,7 @@ fn main() {
     // fn foo(t : dyn Test1){}
     // fn foo(t : dyn Trait){}
     // fn foo(t : *mut dyn Trait){}
+    // fn foo(t : &mut dyn Trait){}
     // fn foo(t : &dyn Trait){}
     // fn foo(t : Box<dyn Trait>){}
     
@@ -182,7 +207,7 @@ fn main() {
     // fn static_foo<T:Trait>(b: T) {}
 
     //15.1
-    //(dyn Trait | &dun Trait) declares that trait type must implement (Sized)
+    //(dyn Trait | &dyn Trait) declares that trait type must implement (Sized)
     //pointers have a size, so we can borrow the trait at compile time
     //wherein the content of the pointer itself might not be sized
     //15.2
@@ -198,47 +223,46 @@ fn main() {
     //15.5
     //(&dyn Trait) is using vtables to fetch the varibale type
     //it could be used only with object-safe traits
-
+    
 
     // trait Trait<'a> where &'a Self: Sized {}
 
-    trait Trait where Self : Sized + Copy{
-        fn bar1(&self){}
-        fn bar2(self){}
-        fn bar3(&mut self){}
+    // trait Trait where Self : Sized + Copy{
+    //     fn bar1(&self){}
+    //     fn bar2(self){}
+    //     fn bar3(&mut self){}
 
-        fn foo(&mut self)
-        {
-            // Self::bar1();
-            // bar1();
-            // self.bar1(&self);
-            // Self::bar1(Self);
+    //     fn foo(&mut self)
+    //     {
+    //         // Self::bar1();
+    //         // bar1();
+    //         // self.bar1(&self);
+    //         // Self::bar1(Self);
 
-            Self::bar1(self);
-            Self::bar1(&self);
-            self.bar1();
-            (&self).bar1();
-            (*self).bar1();
-            // (**self).bar1();
+    //         Self::bar1(self);
+    //         Self::bar1(&self);
+    //         self.bar1();
+    //         (&self).bar1();
+    //         (*self).bar1();
+    //         // (**self).bar1();
 
-            // Self::bar2(self);
-            Self::bar2(*self);
-            self.bar2();
+    //         // Self::bar2(self);
+    //         Self::bar2(*self);
+    //         self.bar2();
             
-            Self::bar3(self);
-            self.bar3();
-        }
-    }
+    //         Self::bar3(self);
+    //         self.bar3();
+    //     }
+    // }
 
-    #[derive(Clone, Copy)]
-    struct Test;
-    impl Trait for Test{}
+    // #[derive(Clone, Copy)]
+    // struct Test;
+    // impl Trait for Test{}
 
-    let t: *mut Test = &mut Test;
-    unsafe{(*t).bar1();}
-    unsafe{(*t).bar2();}
+    // let t: *mut Test = &mut Test;
+    // unsafe{(*t).bar1();}
+    // unsafe{(*t).bar2();}
     
-
     // trait UBTrait {
     //     /*unsafe*/ fn bar(self: *const Self);
     // }
@@ -256,15 +280,90 @@ fn main() {
     //n.4
     //we cannot brake trait vtable using (*const Self) with garbage value
     //because it is prohibited to use raw pointers with Self type
+    //n.5
+    //(self) with (&mut dyn Trait) must be (&mut)
 
+
+    // const fn foo2(){}
+
+    // fn foo<F>(f:F)
+    // where
+    //     F: for<'a> Fn(&'a str, &'a str) -> &'a str
+    // {}
+
+    //n.1
+    //there is (const) functions that behave like constexpr
+    //they are being calculated at compile time
+    //there is also ~const experimantal feature const_fn_trait_bound
+    //n.2
+    //there is the way to declare type lifetime etc using (for) syntax
+
+
+    // let x = fn a(){};
+    // let x = fn(){};
+    
+    // fn t(a: i32){}
+    // fn foo<F,T>(f : /*&*/F, t: T) where F: Fn/*Mut*/(T){
+    //     f(t);
+    // }
+    // foo(/*&*/t, 5);
+
+    // fn foo1(){}
+    // let a = foo1;
+    // let b: &dyn Fn() = &foo1;
+    // b();
+
+    //n.1
+    //originally function pointer type is (fn T())
+    //n.2
+    //function pointer trait is (Fn), (FnOnce) and (FnMut)
+    //(Fn) is for (&self) borrowing
+    //(FnOnce) is for (self) argument which does move operation
+    //(FnMut) is for (&mut self) borrowing
+    //we could implement these traits by overloading (call) operator
+    //n.3
+    //there is shortcut for traits as a return function type
+    //(impl) allows to dynamically implement trait to empty struct
+
+
+    // let mut i = "123".to_string();
+    // let c = |p: i32|{
+    //     i.clear();
+    //     // let a = s;
+    //     // println!("{}",s);
+    // };
+    // // c(1);
+    // let mut f = c;
+    // f(1);
+
+    // fn make_fn() -> impl Fn(){
+    //     let s = "123".to_string();
+    //     move || {
+    //         let v = &s;
+    //     }
+    // }
+
+    //n.1
+    //closure is just a function with current scope visibility
+    //it do not copy variables implicitly, so all variable assign rules works the same
+    //arguments of closure works the same as (fn) arguments except (self) which is no allowed?
+    //n.2
+    //it do implement one of (Fn*) traits implicitly according to borrowing rules
+    //it is (Fn) by default and if using (&) borrowing
+    //it become (FnOnce) if there is move occurs in body
+    //it become (FnMut) if using (&mut) borrowing
+    //(FnMut) closure itself must be a (mut) variable if intended to be called
+    //n.3
+    //if closure is declared with (move) it is trying to move all scope variable in
+    //but closure still might be (FnMut) and (Fn)
 
 
     //n
-    //some magick with fn and Fn and closures here
-
+    //some magick with smart pointers here
 
 
     //n
+    //learn lifetime erasure rules first (https://linkhere)
     //some magic with variance
     //https://doc.rust-lang.org/reference/subtyping.html
 
@@ -299,13 +398,6 @@ fn main() {
     //13.3
     //there is no constructor, new and method overloading
     //so no standart for implementing it
-
-
-
-
-    //n
-    //some magick with smart pointers here
-
 
 
     //n
